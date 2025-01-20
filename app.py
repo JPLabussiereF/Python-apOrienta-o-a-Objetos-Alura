62a99023000749fa337998bf29cf0f1fe0c1eab2
Joao Labussiere <labussework@gmail.com>
2025-01-19 23:23:30 -0300

    Add .gitignore and docstrings

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
 from modelos.restaurante import Restaurante
 
+# Criando instâncias da classe Restaurante e adicionando avaliações
 restaurante_labu = Restaurante('podrão do Labu', 'Podrões')
 restaurante_labu.receber_avaliacao('João', 5)
 restaurante_labu.receber_avaliacao('Maria', 4)
 restaurante_labu.receber_avaliacao('José', 5)
 restaurante_labu.alternar_status()
 restaurante_candinho = Restaurante('pastelaria do Candinho', 'Pastéis')
-restaurante_candinho.receber_avaliacao('Greg', 4.9)
+restaurante_candinho.receber_avaliacao('Greg', 5)
 restaurante_novais = Restaurante('restaurante do Novais', 'Comida Caseira')
+restaurante_novais.receber_avaliacao('Ana', 3)
 
 def main():
+    """
+    Função principal que lista os restaurantes cadastrados e exibe as informações do restaurante 'podrão do Labu'.
+
+    Esta função chama o método de classe `listar_restaurantes` para exibir todos os restaurantes 
+    registrados e, em seguida, imprime os detalhes do restaurante específico `restaurante_labu`.
+
+    Returns:
+        None
+    """
     Restaurante.listar_restaurantes()
-    print(restaurante_labu)
+    print(f'\n{restaurante_labu}')
 
 if __name__ == '__main__':
+    """
+    Ponto de entrada do script.
+
+    Quando o script é executado diretamente, a função `main()` é chamada para listar os restaurantes 
+    e exibir informações adicionais.
+    """
     main()
--- modelos/avaliacao.py
+++ modelos/avaliacao.py
@@ -1,11 +1,41 @@
 class Avaliacao:
+    """
+    Classe que representa uma avaliação de um restaurante feita por um cliente.
+
+    Atributos:
+        cliente (str): Nome do cliente que realizou a avaliação.
+        nota (float): Nota dada pelo cliente, variando de 0.0 a 5.0.
+
+    Métodos:
+        __init__(cliente, nota):
+            Inicializa uma nova avaliação com os dados do cliente e a nota atribuída.
+    """
     def __init__(self, cliente, nota):
+        """
+        Inicializa uma instância da classe Avaliacao.
+
+        Args:
+            cliente (str): Nome do cliente que fez a avaliação.
+            nota (float): Nota atribuída pelo cliente, entre 0.0 e 5.0.
+        """
         self._cliente = cliente,
         self._nota = nota
     
     @property
     def nota(self):
+        """
+        Obtém a nota atribuída na avaliação.
+
+        Returns:
+            float: A nota dada pelo cliente.
+        """
         return self._nota
     @property
     def cliente(self):
+        """
+        Obtém o nome do cliente que fez a avaliação.
+
+        Returns:
+            str: O nome do cliente.
+        """
         return self._cliente
